96ac96e9b6b561cfe16a0793d24dd09a41f1dfb5
ericrsong <ys2731@nyu.edu>
2021-06-08 15:55:28 +0800

    add Fragment

--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -16,12 +16,7 @@
 package com.example.wordsapp
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.databinding.ActivityMainBinding
 
@@ -46,49 +41,7 @@ class MainActivity : AppCompatActivity() {
 //        recyclerView.adapter = LetterAdapter()
 
         // Still need to define the member recycleView
-        recyclerView = binding.recyclerView
-        chooseLayout()
-    }
-
-    private fun chooseLayout() {
-        if (isLinearLayoutManager ) {
-            recyclerView.layoutManager = LinearLayoutManager(this)
-        } else {
-            recyclerView.layoutManager = GridLayoutManager(this, 4)
-        }
-        recyclerView.adapter = LetterAdapter()
-    }
-
-    private fun setIcon(menuItem: MenuItem?) {
-        if (menuItem == null) {
-            return
-        }
-        menuItem.icon =
-        if (isLinearLayoutManager ) {
-            ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
-        } else {
-            ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.layout_menu, menu)
-        val layoutButton = menu?.findItem(R.id.action_switch_layout)
-        setIcon(layoutButton)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            // check if it is the swich layout item that is pressed
-            R.id.action_switch_layout -> {
-                isLinearLayoutManager = !isLinearLayoutManager
-                chooseLayout()
-                setIcon(item)
-                return true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-
+//        recyclerView = binding.recyclerView
+//        chooseLayout()
     }
 }
---
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -0,0 +1,68 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.FragmentWordListBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WordListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WordListFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentWordListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView:RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Retrieve the LETTER from the Intent extras
+        // intent.extras.getString returns String? (String or null)
+        // so toString() guarantees that the value will be a String
+        val letterId = activity?.intent?.extras?.getString(LETTER).toString()
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
+        // Adds a [DividerItemDecoration] between items
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        val LETTER = "letter"
+        val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+}
